9bc90a21af282759d608fdf9635a5a24c3345f71
Allen Porter <allen.porter@gmail.com>
2014-01-01 11:35:33 -0800

    Initial Commit

---
+++ mug/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mug
+//
+//  Created by Allen Porter on 1/1/14.
+//  Copyright (c) 2014 Allen Porter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
